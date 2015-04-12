cb432346b569da0023cc17ee8f20933dbb47d914
Adam Li <adam2392@gmail.com>
2015-04-12 00:39:52 -0700

    Added testing the PID from Lucas to the Tester.c file.

--- Tester.c
+++ Tester.c
@@ -19,8 +19,8 @@ Pins Used:
 #define led3 28
 
 //Pin definitions for motor
-const int R_fwd = 5, R_bkw = 6;  //R_fwd = 1 and R_bkw = 0 -> go forward on rightside
-const int L_fwd = 3, L_bkw = 4;  //L_fwd = 1 and L_bkw = 0 -> go forward on leftside
+const int R_fwd = 6, R_bkw = 5;  //R_fwd = 1 and R_bkw = 0 -> go forward on rightside
+const int L_fwd = 4, L_bkw = 3;  //L_fwd = 1 and L_bkw = 0 -> go forward on leftside
 
 //Sensors
 #define LF_Emitter 23        //left front emitter
@@ -31,6 +31,13 @@ const int L_fwd = 3, L_bkw = 4;  //L_fwd = 1 and L_bkw = 0 -> go forward on left
 #define SIDE_LOW_POWER 22  //turn off Right and Left Emitters
 #define R_Receiver 19    //Right sensor receiver
 #define L_Receiver 18  //Left sensors receiver
+
+// Encoders 
+#define LenchA 8 
+#define LenchB 7
+#define RenchA 10
+#define RenchB 9
+
 
 //Speaker
 #define speakerPin 2
@@ -64,28 +71,68 @@ void writeRightFront(){
 
 // DRIVING Functions for left and right motor ***** Probably needs testing to set pwmvalue
 //turn left motor on or off
-void leftForward(pwmvalue){
+void leftForward(int pwmvalue){
   //digital write for testing/debugging
   analogWrite(L_fwd, pwmvalue);
   analogWrite(L_bkw, LOW);
 }
-void rightForward(pwmvalue){
+void rightForward(int pwmvalue){
   //digital write for testing/debugging
   analogWrite(R_fwd, pwmvalue);
   analogWrite(R_bkw, LOW);
 }
-void leftBackward(pwmvalue){
+void leftBackward(int pwmvalue){
   analogWrite(L_fwd, LOW);
   analogWrite(L_bkw, pwmvalue);
 }
-void rightBackward(pwmvalue){
+void rightBackward(int pwmvalue){
   analogWrite(R_fwd, LOW);
   analogWrite(R_bkw, pwmvalue);  
 }
+ 
+void readEnc(){
+   //rightenc = analogRead(RenchA); 
+   Serial.print("Right EncA: ");
+   Serial.println(digitalRead(RenchA)); 
+   
+   //rightencb = analogRead(RenchB); 
+   Serial.print("Right EncB: ");
+   Serial.println(digitalRead(RenchB)); 
+   Serial.println(" "); 
+   
+   //leftenca = analogRead(LenchA); 
+   Serial.print("Left EncA: ");
+   Serial.println(digitalRead(LenchA));
+  
+   //leftencb = analogRead(LenchB);  
+   Serial.print("Left EncB: ");
+   Serial.println(digitalRead(LenchB));
+}
+void about_face(){
+  
+  rightForward(20); 
+  leftBackward(30); 
+  delay(1000);
+  rightForward(0); 
+  leftForward(0); 
+  
+}
 
+ int rightenca = 0; 
+ int rightencb = 0; 
+ int leftenca = 0; 
+ int leftencb = 0;
+ 
+ 
 // the setup routine runs once when you press reset:
 void setup()
 {
+   
+  pinMode(RenchA, INPUT); 
+  pinMode(RenchB, INPUT);
+  pinMode(LenchA, INPUT); 
+  pinMode(LenchB, INPUT); 
+  
   //initialize motor pins to off
   pinMode(R_fwd, OUTPUT);  //initialize rightmotor forward as output
   pinMode(R_bkw, OUTPUT);
@@ -114,6 +161,9 @@ void setup()
   
   //setup the speaker pin
   pinMode(speakerPin, OUTPUT);
+  
+  rightForward(0); 
+  leftForward(0); 
 }
 
 // the loop routine runs over and over again forever:
@@ -122,13 +172,37 @@ void loop()
   writeSides();  //turn on side emitters to receive distance signal
 
 
-  drive_straight_PID();
+  //drive_straight_PID();
+  //leftForward(30); 
+  //rightForward(30); 
+  //leftBackward(30);
+  //rightBackward(30); 
+  //Serial.print("IR left diag: ");
+  //Serial.println(readLeft());
+  //Serial.print("IR right diag: "); 
+  //Serial.println(readRight()); 
+  //drive_test(); 
+  //drive_straight_PID(); 
+  readEnc(); 
+  //about_face(); 
   delay(100);
 }
-
+void drive_test(){
+  int leftDistance = readLeft(); 
+  
+  if(leftDistance > 80) {
+    leftBackward(30); 
+    rightBackward(30); 
+  }  
+  else if( leftDistance < 70 ) {
+    rightForward(30); 
+    leftForward(30);  
+  }
+  
+}
 /**** PID TEST ****/
 void drive_straight_PID(void){
-  int offset = 556;
+  int offset = -40;
   static int previous_error = 0;
   static int previous_time = 0;
   static int last_big = 0;
@@ -159,16 +233,29 @@ void drive_straight_PID(void){
   }
   total = error *kp;
   previous_time = current_time;
+  
   //analogWrite(R_fwd, HIGH - total);
   //analogWrite(L_fwd, HIGH + total);
   //what the PID will do (because motor functions are not done)
+  if(total > 25)
+    total=0.5*total; 
+  if(total > 50 )
+    total = 0; 
+  if(total<-50)
+    total=0;
+  Serial.print("total error: "); 
+  Serial.println(total); 
+  rightForward(15+total); 
+  leftForward(25-total); 
   if( error == 0 ){
     Serial.print(" Mouse is straight: ");
     Serial.println(error);
+     
   }
   if( error > 0 ){
     Serial.print(" Mouse is veering right: ");
     Serial.println(error);
+    
   }
   if( error < 0 ){
     Serial.print(" Mouse is veering left: ");
